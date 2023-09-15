ef2b1d1a2c183f63d0b2e72029f9a9a86f83e8d6
Wanderson Klebert <101153218+WandersonKleberth@users.noreply.github.com>
2023-09-15 18:10:58 -0300

    adicionando um botao para voltar ao formulario
    botao para voltar ao formulario apos termino de cadastro do recruta

--- register-script.js
+++ register-script.js
@@ -15,7 +15,9 @@ function handleSubmit(event) {
     <h1>Informações do Recruta</h1>
     <p>Nome: ${firstName.toUpperCase()} ${lastName.toUpperCase()}</p>
     <p>Campo de Estudo: ${fieldOfStudy.toUpperCase()}</p>
-    <p>Idade: ${age} anos</p>`;
+    <p>Idade: ${age} anos</p>
+    <button onclick="backToForm()">Voltar ao formulário</button>
+    `;
 
     document.querySelector(".card").innerHTML = result;
 }
@@ -25,6 +27,37 @@ document.querySelector("form").addEventListener("submit", handleSubmit);
 function calculateAge (yearOfBirth) {
     const thisYear = new Date().getFullYear();
     return thisYear - yearOfBirth;
+}
+
+function backToForm() {
+    const form = `
+    <h1>Cadastro de Recrutas</h1>
+    <h2>Preencha suas informações</h2>
+    <form>
+        <div class="form-group">
+            <label for="firstName">Primeiro Nome:</label>
+            <input type="text" id="firstName" name="firstName" placeholder="Digite seu primeiro nome" required>
+        </div>
+        <div class="form-group">
+            <label for="lastName">Sobrenome:</label>
+            <input type="text" id="lastName" name="lastName" placeholder="Digite seu sobrenome" required>
+        </div>
+        <div class="form-group">
+            <label for="fieldOfStudy">Campo de Estudo:</label>
+            <input type="text" id="fieldOfStudy" name="fieldOfStudy" placeholder="Digite seu campo de estudo"
+                required>
+        </div>
+        <div class="form-group">
+            <label for="birthDate">Ano de Nascimento:</label>
+            <input type="date" id="birthDate" name="birthDate" required>
+        </div>
+        <div class="form-group text-center"> <!-- Novo elemento div para centralizar o botão -->
+            <button type="submit">Enviar</button>
+        </div>
+    </form>
+    `;
+    
+    document.querySelector(".card").innerHTML = form;
 }
 
 //Aqui está uma forma um pouco mais simples do codigo JS
